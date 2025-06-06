28cb3e2f4ba178f28408b0509ddd421d2c7df033
Anh Nguyn <anhn56423@gmail.com>
2025-06-06 23:14:09 +0700

    set up express

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello, World! This is a simple Node.js application.");
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+
+//chạy ứng dụng
+
+app.listen(8080, ()=>{
+    console.log(`Server is running on port: ${PORT}`);
+})
